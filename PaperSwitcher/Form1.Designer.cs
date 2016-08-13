f385fbc51ce7bb54391e24d844be448c30a23006
fireantik <windowmaker001@gmail.com>
2016-08-13 14:48:57 +0200

    Add project files.

---
+++ PaperSwitcher/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace PaperSwitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "PaperSwitcher";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(6, 69);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(273, 28);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 54);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 46);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wallpaper folder";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(204, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 28);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(6, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnNext);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(285, 102);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Action";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnStart);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 65);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(285, 46);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Interval";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(204, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10 seconds",
+            "30 seconds",
+            "1 minute",
+            "5 minutes",
+            "10 minutes",
+            "15 minutes",
+            "30 minutes",
+            "1 hour",
+            "2 hours",
+            "5 hours",
+            "12 hours",
+            "24 hours"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.nextToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nextToolStripMenuItem.Text = "Next";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.nextToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 226);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PaperSwitcher";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    }
+}
+
---
+++ PaperSwitcher/Form1.cs
@@ -0,0 +1,297 @@
+﻿using PaperSwitcher.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaperSwitcher
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo WallpaperFolder;
+        bool WallpaperFolderGood = false;
+        bool Running = false;
+        TimeSpan Interval = TimeSpan.Zero;
+        int MonitorCount => Screen.AllScreens.Count();
+        FileInfo[] Current = new FileInfo[0];
+        HashSet<string> Seen = new HashSet<string>();
+
+        Settings Config => Settings.Default;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Config.Seen = Config.Seen ?? new System.Collections.Specialized.StringCollection();
+            MinimumSize = Size;
+
+            foreach (var item in Config.Seen)
+            {
+                Seen.Add(item);
+            }
+
+            textBox1.Text = Config.Path;
+            comboBox1.SelectedIndex = Config.SelectedInterval;
+
+            if (WallpaperFolderGood)
+            {
+                WindowState = FormWindowState.Minimized;
+                Hide();
+                StartRunning();
+            }
+
+            ReloadEnabled();
+        }
+
+        void ReloadEnabled()
+        {
+            if (!WallpaperFolderGood) StopRunning();
+            btnStart.Enabled = comboBox1.Enabled = WallpaperFolderGood;
+            label1.Enabled = btnNext.Enabled = Running;
+            btnStart.Text = Running ? "Stop" : "Start";
+            this.Text = notifyIcon1.Text = Running ? "PaperSwitcher - Running" : "PaperSwitcher";
+        }
+
+        void StartRunning()
+        {
+            Next();
+            Redisplay();
+            Running = true;
+        }
+
+        void StopRunning()
+        {
+            Running = false;
+        }
+
+        void ScalePic(Image pic, Graphics target, int width, int height)
+        {
+            var ratio = pic.Height * 1.0 / pic.Width;
+
+            int outH = height;
+            int outW = (int)(outH / ratio);
+
+            if (outW < width)
+            {
+                outW = width;
+                outH = (int)(width * ratio);
+            }
+
+            var offsetX = (outW - width) / 2;
+            var offsetY = (outH - height) / 2;
+
+            var rect = new Rectangle(-offsetX, -offsetY, outW, outH);
+            target.DrawImage(pic, rect);
+        }
+
+        void Redisplay()
+        {
+            label1.Text = string.Join("\n", Current.Select(f => f.Name));
+            var xMin = Screen.AllScreens.Min(s => s.Bounds.Left);
+            var yMin = Screen.AllScreens.Min(s => s.Bounds.Top);
+            var xMax = Screen.AllScreens.Max(s => s.Bounds.Right);
+            var yMax = Screen.AllScreens.Max(s => s.Bounds.Bottom);
+
+            var filename = Path.GetTempFileName() + ".bmp";
+
+            using (var img = new Bitmap(xMax - xMin, yMax - yMin))
+            {
+                using (var g = Graphics.FromImage(img))
+                {
+                    for (int i = 0; i < Screen.AllScreens.Length; i++)
+                    {
+                        var screen = Screen.AllScreens[i];
+                        using (Image scaled = new Bitmap(screen.Bounds.Width, screen.Bounds.Height))
+                        {
+                            using (var sg = Graphics.FromImage(scaled))
+                            {
+                                try
+                                {
+                                    var pic = Image.FromFile(Current[i].FullName);
+                                    ScalePic(pic, sg, screen.Bounds.Width, screen.Bounds.Height);
+                                }
+                                catch { }
+                            }
+
+                            var rect = new Rectangle(screen.Bounds.Left - xMin, screen.Bounds.Top - yMin, screen.Bounds.Width, screen.Bounds.Height);
+                            g.DrawImage(scaled, rect);
+                        }
+                    }
+                }
+                img.Save(filename);
+            }
+
+            Wallpaper.Set(filename, Wallpaper.Style.Tiled);
+        }
+
+        FileInfo[] GetImagesIn(DirectoryInfo path)
+        {
+            return WallpaperFolder.GetFiles("*.jpg")
+                .Concat(WallpaperFolder.GetFiles("*.jpeg"))
+                .Concat(WallpaperFolder.GetFiles("*.png"))
+                .Concat(WallpaperFolder.GetFiles("*.bmp"))
+                .OrderBy(i => i.Name)
+                .ToArray();
+        }
+
+        void Next()
+        {
+            var images = GetImagesIn(WallpaperFolder);
+
+            if(Seen.Count + MonitorCount > images.Length)
+            {
+                Seen.Clear();
+                Config.Seen.Clear();
+            }
+
+            int left = MonitorCount;
+            Current = new FileInfo[MonitorCount];
+            Random r = new Random();
+
+            while (left != 0)
+            {
+                var img = images[r.Next(0, images.Length - 1)];
+                if (Seen.Contains(img.Name)) continue;
+
+                Current[left - 1] = img;
+                Seen.Add(img.Name);
+                Config.Seen.Add(img.Name);
+                left--;
+            }
+
+            Config.Save();
+            Redisplay();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Config.Path != textBox1.Text)
+            {
+                Config.Path = textBox1.Text;
+                Config.Save();
+            }
+
+            try
+            {
+                WallpaperFolder = new DirectoryInfo(textBox1.Text);
+                if (!WallpaperFolder.Exists) throw new Exception();
+                if (GetImagesIn(WallpaperFolder).Length < MonitorCount) throw new Exception();
+                WallpaperFolderGood = true;
+            }
+            catch (Exception)
+            {
+                WallpaperFolderGood = false;
+            }
+
+            ReloadEnabled();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Config.SelectedInterval != comboBox1.SelectedIndex)
+            {
+                Config.SelectedInterval = comboBox1.SelectedIndex;
+                Config.Save();
+            }
+
+            var splat = comboBox1.SelectedItem.ToString().Split(' ');
+            var period = int.Parse(splat[0]);
+            var name = splat[1];
+
+            switch (name)
+            {
+                case "second":
+                case "seconds":
+                    Interval = new TimeSpan(0, 0, period);
+                    break;
+
+                case "minute":
+                case "minutes":
+                    Interval = new TimeSpan(0, period, 0);
+                    break;
+
+                case "hour":
+                case "hours":
+                    Interval = new TimeSpan(period, 0, 0);
+                    break;
+
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+
+            timer1.Stop();
+            timer1.Interval = (int)(Interval.TotalMilliseconds);
+            timer1.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Running = !Running;
+            ReloadEnabled();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            Next();
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            BringToFront();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized) Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!Running) return;
+
+            Next();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            contextMenuStrip1.Show(MousePosition);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void nextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Next();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            BringToFront();
+        }
+    }
+}
